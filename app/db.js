0bdfa7ba254a8ddcdc480a56b8cd824abd516326
tmlewallen <tmlewallen@crimson.ua.edu>
2016-12-10 11:47:30 -0500

    deploy commit

--- app/db.js
+++ app/db.js
@@ -2,9 +2,14 @@
  * Created by Tmlewallen on 5/8/16.
  */
 
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/GoalTracker');
+let mongoose = require('mongoose');
+let deets = require('./secrets');
+if (deets.local){
+    mongoose.connect('mongodb://localhost/GoalTracker');
+}
+else {
+    mongoose.connect(`mongodb://${deets.username}:${deets.password}${deets.url}`);
+}
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
