8ef17eb3b7aa3f97f6abef44c4809150f78ca065
tmlewallen <tmlewallen@crimson.ua.edu>
2016-12-19 18:48:12 -0500

    added a deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 cp app/secrets.js app/secrets-prod.js
 git add app/secrets-prod.js
 git commit -m "BUILD AUTO-COMMIT"
-heroku config:set ENV:PROD
-git push heroku master
-git reset --hard HEAD^
+heroku config:set ENV=PROD
+git push -f heroku master
+git reset HEAD^
 rm app/secrets-prod.js
